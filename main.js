1cbfb0f0bdd208a9b07379125683be61712a0261
mithular <rameshkumarmithula@gmail.com>
2019-02-11 11:37:17 +0530

    fun

--- main.js
+++ main.js
@@ -1,16 +1,97 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
-$(document).ready(function() {
- SC.stream('/tracks/494022999',function(sound){
- $('#start').click(function(e) {
-            e.preventDefault();
-            sound.start();
-          });
-          $('#stop').click(function(e) {
-                   e.preventDefault();
-                   sound.stop();
-                 });
 
+
+$(document).ready(function()   {
+
+  SC.stream('/tracks/345885029', function(sound) {
+  $('#start1').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#stop1').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
 });
+// hoshana
+ SC.stream('/tracks/519204183', function(sound) {
+  $('#start2').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#stop2').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+// rasaali
+ SC.stream('/tracks/345819962', function(sound) {
+  $('#start3').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#stop3').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+// usure poguthu
+SC.stream('/tracks/346104528', function(sound) {
+ $('#start4').click(function(e) {
+   e.preventDefault();
+   sound.start();
  });
+ $('#stop4').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+// naan un alaginele
+SC.stream('/tracks/317969885', function(sound) {
+ $('#start5').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop5').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+//yenai mattrum kadhale
+SC.stream('/tracks/438430806', function(sound) {
+ $('#start6').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop6').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+//laali laali
+
+SC.stream('/tracks/524553387', function(sound) {
+ $('#start7').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop7').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+//vayaadi petha pulla
+SC.stream('/tracks/494023014', function(sound) {
+ $('#start8').click(function(e) {
+   e.preventDefault();
+   sound.start();
+ });
+ $('#stop8').click(function(e) {
+   e.preventDefault();
+   sound.stop();
+ });
+});
+//neeyum nanum anbe
+});
